464029fac0ef198192776dc0e2474d6673c1239d
“Isaac <2657998G@student.gla.ac.uk>
2023-01-26 16:16:38 +0000

    Complete chatper 9

--- rango/forms.py
+++ rango/forms.py
@@ -1,9 +1,10 @@
 from django import forms
-from rango.models import Page, Category
-
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
+MAX_LENGTH = 128
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
+    name = forms.CharField(max_length=MAX_LENGTH,
     help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -15,7 +16,7 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
+    title = forms.CharField(max_length=MAX_LENGTH,
     help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,
     help_text="Please enter the URL of the page.")
@@ -30,3 +31,12 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
             return cleaned_data
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -94,6 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -127,4 +128,11 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+LOGIN_URL = 'rango:login'
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
